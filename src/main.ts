9409adf837638b9daa2467fa14885cd648b7d4bb
rap001 <raheesrad8@gmail.com>
2025-05-29 22:33:22 +0530

    updated url to public

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // <--- This is the crucial line!
+    origin: process.env.CORS_URL, // <--- This is the crucial line!
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Include all HTTP methods you use
     credentials: true, // Set to true if you are sending cookies or authorization headers (e.g., JWT)
   });
