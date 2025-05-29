ec9bb6151882f981962ca0d28b2aec012364eb64
rap001 <raheesrad8@gmail.com>
2025-05-30 00:59:00 +0530

    edited

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_URL??'https://job-portal-frontend-ashy-six.vercel.app/', // <--- This is the crucial line!
+    origin: process.env.CORS_URL??'https://job-portal-frontend-ashy-six.vercel.app', // <--- This is the crucial line!
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Include all HTTP methods you use
     credentials: true, // Set to true if you are sending cookies or authorization headers (e.g., JWT)
   });
